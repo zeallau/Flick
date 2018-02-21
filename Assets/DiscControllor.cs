8b2663012a69373968ef6b8bf0c8a498757a3679
zeallau <lauzeal@gmail.com>
2018-02-21 19:28:01 +0900

    2nd push
    Added TestDisc to test moving.

--- Assets/DiscControllor.cs
+++ Assets/DiscControllor.cs
@@ -6,37 +6,42 @@ public class DiscControllor : MonoBehaviour {
 
     private Vector3 touchStartPos;
     private Vector3 touchEndPos;
-
+    
 
     // Use this for initialization
     void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         Flick();
 	}
 
+    //Moving direction
     void Flick()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Debug.Log("ClickDown");
-            
+                       
             touchStartPos = new Vector3(Input.mousePosition.x,
                                         Input.mousePosition.y,
                                         Input.mousePosition.z);
+
+            Debug.Log(touchStartPos);
                                         
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
-            Debug.Log("ClickUp");
-            
+                      
             touchEndPos = new Vector3(Input.mousePosition.x,
                                       Input.mousePosition.y,
                                       Input.mousePosition.z);
+
+            Debug.Log(touchEndPos);
+
             GetDirection();
             
         }
@@ -44,49 +49,23 @@ public class DiscControllor : MonoBehaviour {
 
     void GetDirection()
     {
-        Debug.Log("hello");
-        
         float directionX = touchEndPos.x - touchStartPos.x;
         float directionY = touchEndPos.y - touchStartPos.y;
-        string Direction;
 
-        if (Mathf.Abs(directionY) < Mathf.Abs(directionX))
-        {
-            if (30 < directionX)
-            {
-                //右向きにフリック
-                Direction = "right";
+        //.normalized用來限制移動半徑為1
+        Vector3 dir = new Vector3(directionX, directionY, 0.0f).normalized;
+                
+        // Move to the same direction as directionX, directionY
+        this.gameObject.transform.Translate(dir);
 
-                Debug.Log("Right");
-            }
-            else if (-30 > directionX)
-            {
-                //左向きにフリック
-                Direction = "left";
-            }
-        }
-
-        else if (Mathf.Abs(directionX) < Mathf.Abs(directionY)){
-            if (30 < directionY)
-            {
-                //上向きにフリック
-                Direction = "up";
-            }
-            else if (-30 > directionY)
-            {
-                //下向きのフリック
-                Direction = "down";
-            }
-        }
-        else
-        {
-            //タッチを検出
-            Direction = "touch";
-        }
-        
-
+        GoDistance();
     }
 
-    
+    void GoDistance()
+    {
+        float distanceX = (touchEndPos.x - touchStartPos.x) * 10;
+        float distanceY = (touchEndPos.y - touchStartPos.y) * 10;
 
+
+    }
 }
---
+++ Assets/TestMoveScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMoveScript : MonoBehaviour {
+
+    private Vector3 StartPos;
+    private Vector3 EndPos;
+    private Transform myTransform;
+
+
+    // Use this for initialization
+    void Start () {
+        this.myTransform = GetComponent<Transform>();
+
+        StartPos = new Vector3(2, -4, 0);
+        this.gameObject.transform.position = StartPos;
+
+        EndPos = new Vector3(2, 4, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        if(myTransform.position.y >= StartPos.y && myTransform.position.y <= EndPos.y)
+        {
+            this.gameObject.transform.Translate(0, 0.1f, 0);
+
+        }
+        
+		
+	}
+}
