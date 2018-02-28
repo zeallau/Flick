88dbbee1dc73252bce915295a45d2729c68270c2
zeallau <lauzeal@gmail.com>
2018-02-28 18:37:37 +0900

    5th push
    finished the basic target

---
+++ Assets/TargetScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour {
+
+    private Vector3 targetPos;
+    private GameObject disc;
+
+    // Use this for initialization
+    void Start () {
+        targetPos = this.gameObject.transform.position;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RightClick();
+
+        disc = GameObject.Find("TestDisc");
+
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        Debug.Log("target hit");
+        Debug.Log("targetPos.Distance is" + Vector3.Distance(disc.transform.position, targetPos));
+    }
+
+  
+
+
+    
+    private void RightClick()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Debug.Log("targetPos is" + targetPos) ;
+            
+        }
+    }
+    
+}
--- Assets/TestMoveScript.cs
+++ Assets/TestMoveScript.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestMoveScript : MonoBehaviour {
-
+public class TestMoveScript : MonoBehaviour
+{
     
     private Vector3 touchStartPos;
     private Vector3 touchEndPos;
@@ -12,12 +12,13 @@ public class TestMoveScript : MonoBehaviour {
     private Vector3 clickMoveDistance;
     private Vector3 objectMoveDistance;
     private Vector3 inititalPos;
+    private float movedDistance = 0.0f;//★追加 移動した距離
 
     private const float MOVE_SPEED_PER_SECOND = 2.0f;
 
     // further feather: object slow down before stop
-     
-    
+
+
     // Use this for initialization
     void Start()
     {
@@ -41,35 +42,42 @@ public class TestMoveScript : MonoBehaviour {
         further feather: setup gameOjbect move distance =  clickMoveDistance * 2.5
         */
 
-        if (this.gameObject.transform.position.y <= objectMoveDistance.y)
-        {
-            //Object moving direction same as clicking, should be fine to remain
+        //if (this.gameObject.transform.position.y <= objectMoveDistance.y) //★削除
+        //{
+        if (movedDistance < clickMoveDistance.magnitude)
+        { //★追加 移動距離がclickMoveDistanceの長さより短い場合
+          //Object moving direction same as clicking, should be fine to remain
+            float moveDistance = (clickMoveDistance * (MOVE_SPEED_PER_SECOND * Time.deltaTime)).magnitude;
             this.gameObject.transform.Translate(clickMoveDistance * (MOVE_SPEED_PER_SECOND * Time.deltaTime));
-            
+            movedDistance += moveDistance;
         }
+
+
+
+
 
         //Question: Right Click to restart position, but it will keep going after Right Click
         Restart();
 
-        
+
     }
 
     //Moving direction
     void Flick()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            
+
 
             touchStartPos = new Vector3(Input.mousePosition.x,
                                         Input.mousePosition.y,
                                         Input.mousePosition.z);
-                        
+
             touchStartworldPos = Camera.main.ScreenToWorldPoint(touchStartPos);
             Debug.Log("touchStarworldPos is" + touchStartworldPos);
 
-            
+
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
@@ -78,21 +86,22 @@ public class TestMoveScript : MonoBehaviour {
             touchEndPos = new Vector3(Input.mousePosition.x,
                                       Input.mousePosition.y,
                                       Input.mousePosition.z);
-                       
+
             touchEndworldPos = Camera.main.ScreenToWorldPoint(touchEndPos);
             Debug.Log("touchEndworldPos is" + touchEndworldPos);
 
             //object move distance is same as click down to click up distance
-            clickMoveDistance = touchEndworldPos - touchStartworldPos;
-            Debug.Log("clickMoveDistance is" + clickMoveDistance);
+            clickMoveDistance = (touchEndworldPos - touchStartworldPos);
+            Debug.Log("clickMoveDistance is" + clickMoveDistance.magnitude);
+            
 
             objectMoveDistance = new Vector3(this.gameObject.transform.position.x + clickMoveDistance.x,
                                              this.gameObject.transform.position.y + clickMoveDistance.y,
                                              this.gameObject.transform.position.z + clickMoveDistance.z);
             Debug.Log("objectMoveDistance is" + objectMoveDistance);
 
+            movedDistance = 0.0f; //★追加 移動距離を０に初期化 
 
-            
         }
     }
 
@@ -107,8 +116,10 @@ public class TestMoveScript : MonoBehaviour {
             //Question: I try to reset position and remain no moving, but it does not work
             objectMoveDistance = new Vector3(0.0f, 0.0f, 0.0f);
             this.gameObject.transform.Translate(0.0f, 0.0f, 0.0f);
+            //★追加 clickMoveDistanceの長さを０にして、移動させないようにする
+            clickMoveDistance = new Vector3(0.0f, 0.0f, 0.0f);
         }
 
     }
-  
+
 }
